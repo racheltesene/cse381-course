3e8bd9431d286a58b67754a627e8268ff5407f66
racheltesene <tes21002@byui.edu>
2024-09-24 16:47:24 -0600

    finally donw

--- Assignments/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ Assignments/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a16be3b949967a603d359f80ab6293bcc24ac53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af98ac857d8fcc8ced03b192b30ac0b21c335850")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
