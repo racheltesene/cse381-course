0545ed0bfb49739bc472401a504539b8282eb409
racheltesene <tes21002@byui.edu>
2024-10-03 12:04:49 -0600

    merge sort 2

--- Assignments/AlgorithmLib/obj/Debug/net8.0/AlgorithmLib.AssemblyInfo.cs
+++ Assignments/AlgorithmLib/obj/Debug/net8.0/AlgorithmLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06bd53c935a85a0cc3cc851b5da3f72c1671b441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8732b6a02b819336d4ff25e0f32eea6a545efc1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
