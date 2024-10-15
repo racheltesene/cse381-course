641242c3e462c9f8afea5f5ed9ba8bc23fd8f680
racheltesene <tes21002@byui.edu>
2024-10-14 19:39:09 -0600

    wo

--- Assignments/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ Assignments/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0545ed0bfb49739bc472401a504539b8282eb409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60935c81a21c71aab757dfc19e63e82200685cb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
